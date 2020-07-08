505e3aa54a30ad31845eb95bf164ac27cb540052
Sonsaky <yu-gi-oh08@inbox.ru>
2020-07-08 20:35:01 +0300

    цикл for для трех значений

---
+++ Основы программирования/5. Цикл for.js
@@ -0,0 +1,13 @@
+/* 5. Сумма всех атуральных чисел, меньших чем (не включительно) n (первый аргумент),
+ которые делятся на числа a или b (второй и третий аргументы) без остатка*/
+ const sum = (n, a, b) => {
+    let result = 0;
+    for (let counter = 1; counter < n; counter += 1) {
+      if ((counter % a) === 0 || (counter % b) === 0) {
+        result += counter;
+      }
+    }
+    return result;
+  };
+  export default sum;
+  
