baae62f17a835023068a85634b1a5312a032647d
Sonsaky <yu-gi-oh08@inbox.ru>
2020-07-09 20:14:07 +0300

    калькулятор, bcgjkmpe. switch

---
+++ Основы программирования/6. Цикл Switch.js
@@ -0,0 +1,17 @@
+// 6. Калькулятор, использую цикл switch
+const calc = (a1, a, b) => {
+    switch (a1) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b
+      case '/':
+        return a / b;
+      case '*':
+        return a * b;
+      default:
+        return NaN;
+    }
+  };
+  export default calc;
+  
