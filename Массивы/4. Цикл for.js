29c18c7aad4257309498a86fab9c68c365cc54c6
Sonsaky <yu-gi-oh08@inbox.ru>
2020-07-14 21:00:40 +0300

    цикл for

---
+++ Массивы/4. Цикл for.js
@@ -0,0 +1,10 @@
+/* 4. Функция, которая принимает на вход массив и строковой префикс, и возвращает новый массив, 
+в котором к каждому элементу исходного массива добавляется переданный префикс */
+const arrays = (arr, pref) => {
+    let newName = arr.slice(0);
+    for (let i = 0; i < arr.length; i += 1) {
+      newName[i] = pref + ' ' + newName[i]; 
+    }
+    return newName;
+};
+export default arrays;
